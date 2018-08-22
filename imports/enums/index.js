88cda373082baa9e3a428a49210c06fe65353264
Roman Krasiuk <rokrassyuk@gmail.com>
2018-08-22 10:37:12 +0300

    remove api keys

--- imports/enums/index.js
+++ imports/enums/index.js
@@ -1,5 +1,1 @@
-export const API_URL = 'https://demo.api.afex.com:7885/Token';
 
-export const API_KEY = '000055B8Vf3508598-df9f-e811-a8b3-0050569b0074';
-
-export const API_PASSWORD = '26a7bb51-4c9f-e811-80d4-0050569b43ff';
