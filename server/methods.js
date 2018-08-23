509f95be05e7aaf4c94b39b0c4c5a44ace8580f2
Roman Krasiuk <rokrassyuk@gmail.com>
2018-08-23 17:08:19 +0300

    remove api call

--- server/methods.js
+++ server/methods.js
@@ -3,16 +3,5 @@ import qs from 'qs';
 import {API_URL, API_KEY, API_PASSWORD} from 'enums';
 
 Meteor.methods({
-  getToken: async () => {
-    const data = await axios.post(API_URL,
-      qs.stringify({
-        Grant_Type: 'password',
-        Username: API_KEY,
-        Password: API_PASSWORD,
-      }),
-      {
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      });
-    return data.data;
-  },
+
 });
