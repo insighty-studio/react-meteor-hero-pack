7f78cc20276b6591315647010094b84529952664
Roman Krasiuk <rokrassyuk@gmail.com>
2018-08-23 16:56:09 +0300

    do not display access token

--- client/app/index.js
+++ client/app/index.js
@@ -18,7 +18,7 @@ class App extends Component {
     console.log(typeof this.state.token, this.state.token)
     return (
       <div className="app">
-        <p>LOLOLOLOLOLO {this.state.token.access_token}</p>
+        <p>LOLOLOLOLOLO</p>
       </div>
     );
   }
