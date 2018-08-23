28a61a309817cf1b0b0af80a8c273cef16b57f3c
Roman Krasiuk <rokrassyuk@gmail.com>
2018-08-23 17:09:04 +0300

    remove method call

--- client/app/index.js
+++ client/app/index.js
@@ -1,25 +1,10 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-  state = {
-    token: {},
-  };
-
-  async componentDidMount() {
-    await Meteor.call('getToken', {}, (err, res) => {
-      if (err) {
-        console.log(err);
-      }
-      this.setState({token: res});
-    });
-  }
-
   render() {
     console.log(typeof this.state.token, this.state.token)
     return (
-      <div className="app">
-        <p>LOLOLOLOLOLO</p>
-      </div>
+      <div className="app" />
     );
   }
 }
