35cdf415e966b47f814ff1f65c4be23d48410782
Roman Krasiuk <rokrassyuk@gmail.com>
2018-08-23 17:09:28 +0300

    remove console log

--- client/app/index.js
+++ client/app/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-    console.log(typeof this.state.token, this.state.token)
     return (
       <div className="app" />
     );
